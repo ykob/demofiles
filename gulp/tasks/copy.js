a16c6b6955e19bc2108985bf86833edda0d2008a
yoichi kobayashi <info@tplh.net>
2016-09-05 18:07:40 +0900

    [fix] missing opts param at gulp copy task.

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -3,11 +3,11 @@ const gulp = require('gulp');
 const conf = require('../conf').copy;
 
 gulp.task('copy-to-dest', () => {
-  return gulp.src(conf.dest.src)
+  return gulp.src(conf.dest.src, conf.dest.opts)
     .pipe(gulp.dest(conf.dest.dest));
 });
 
 gulp.task('copy-to-build', () => {
-  return gulp.src(conf.build.src)
+  return gulp.src(conf.build.src, conf.build.opts)
     .pipe(gulp.dest(conf.build.dest));
 });
